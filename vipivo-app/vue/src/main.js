2a667dee8467df329454ab51d3ef0a4c9ee0db44
emresepetci <yemresepetci@gmail.com>
2023-12-29 10:08:23 +0300

    part 1

---
+++ vipivo-app/vue/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import store from './store'
+import './index.css'
+import router from './router'
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount('#app')
+
+
---
+++ vipivo-app/vue/src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from '../views/Dashboard.vue';
+import Register from '../views/Register.vue';
+import Login from '../views/Login.vue';
+import DefaultLayout from '../components/DefaultLayout.vue';
+
+
+const routes = [
+  {
+  path: '/',
+  redirect: '/dashboard',
+  name: 'DefaultLayout',
+  component: DefaultLayout,
+  children: [
+    {
+      path: '/dashboard', name: 'Dashboard', component: Dashboard
+    },
+  ]
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router;
